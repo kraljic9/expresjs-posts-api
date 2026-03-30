138c7da0519ac6bd1bc1b8d1b5e9ba9dae26f938
Pavle Kraljic <kraljicpavle9@gmail.com>
2026-03-30 20:27:35 +0200

    Added two get routs one will fetch posts array and the other one will fetch a individual post, also added error handeling incasse single post is not found

--- server.js
+++ server.js
@@ -3,12 +3,33 @@ import express from "express";
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-let post = [
+let posts = [
   { id: 1, title: "Post one" },
   { id: 2, title: "Post two" },
   { id: 3, title: "Post three" },
   { id: 4, title: "Post one" },
 ];
+
+// Get all posts
+
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
+
+// Get individual post
+
+app.get("/api/posts/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res
+      .status(404)
+      .json({ message: `Error could not find post with id ${id}` });
+  }
+
+  res.status(200).json(post);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
